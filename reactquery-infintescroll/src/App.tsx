761897174843a66f9eac152fc600594d0750d85c
엔코 이숙영 <sookyoung.lee@enkor.kr>
2022-05-01 14:09:58 +0900

    Set reactquery

--- reactquery-infintescroll/src/App.tsx
+++ reactquery-infintescroll/src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Main from "./pages/Main/Main";
 
 function App() {
-  return <></>;
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { suspense: true } },
+  });
+
+  return (
+    <main>
+      <QueryClientProvider client={queryClient}>
+        <React.Suspense fallback="Loading">
+          <Main />
+        </React.Suspense>
+      </QueryClientProvider>
+    </main>
+  );
 }
 
 export default App;
